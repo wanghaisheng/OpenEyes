bd747cfbfefc6672c888defef6ab479142e8682e
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-01-21 08:52:44 +0000

    [OE-2546] added migration code for complications

--- protected/migrations/m130121_083227_benefits_and_risks_should_be_linked_to_subspecialty_rather_than_service.php
+++ protected/migrations/m130121_083227_benefits_and_risks_should_be_linked_to_subspecialty_rather_than_service.php
@@ -16,6 +16,19 @@ class m130121_083227_benefits_and_risks_should_be_linked_to_subspecialty_rather_
 
 		$this->createIndex('procedure_benefit_subspecialty_id_fk','procedure_benefit','subspecialty_id');
 		$this->addForeignKey('procedure_benefit_subspecialty_id_fk','procedure_benefit','subspecialty_id','subspecialty','id');
+
+		$this->dropForeignKey('procedure_complication_service_id_fk','procedure_complication');
+		$this->dropIndex('procedure_complication_service_id_fk','procedure_complication');
+
+		$this->renameColumn('procedure_complication','service_id','subspecialty_id');
+
+		foreach (Service::model()->findAll() as $service) {
+			$ssa = ServiceSubspecialtyAssignment::model()->find('service_id=?',array($service->id));
+			$this->update('procedure_complication',array('subspecialty_id'=>$ssa->subspecialty_id),'subspecialty_id='.$service->id);
+		}
+
+		$this->createIndex('procedure_complication_subspecialty_id_fk','procedure_complication','subspecialty_id');
+		$this->addForeignKey('procedure_complication_subspecialty_id_fk','procedure_complication','subspecialty_id','subspecialty','id');
 	}
 
 	public function down()
@@ -32,5 +45,18 @@ class m130121_083227_benefits_and_risks_should_be_linked_to_subspecialty_rather_
 
 		$this->createIndex('procedure_benefit_service_id_fk','procedure_benefit','service_id');
 		$this->addForeignKey('procedure_benefit_service_id_fk','procedure_benefit','service_id','service','id');
+
+		$this->dropForeignKey('procedure_complication_subspecialty_id_fk','procedure_complication');
+		$this->dropIndex('procedure_complication_subspecialty_id_fk','procedure_complication');
+
+		$this->renameColumn('procedure_complication','subspecialty_id','service_id');
+
+		foreach (Subspecialty::model()->findAll() as $subspecialty) {
+			$ssa = ServiceSubspecialtyAssignment::model()->find('subspecialty_id=?',array($subspecialty->id));
+			$this->update('procedure_complication',array('service_id'=>$ssa->service_id),'service_id='.$subspecialty->id);
+		}
+
+		$this->createIndex('procedure_complication_service_id_fk','procedure_complication','service_id');
+		$this->addForeignKey('procedure_complication_service_id_fk','procedure_complication','service_id','service','id');
 	}
 }
