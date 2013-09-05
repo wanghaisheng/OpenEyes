685320e0d9514c7860e47166ba583cd88dea868c
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-09-05 10:01:04 +0100

    tweak to support nowrapper mode with text-align = right

--- protected/widgets/views/CheckBox.php
+++ protected/widgets/views/CheckBox.php
@@ -18,11 +18,13 @@
  */
 ?>
 <?php if (@$options['text-align'] == 'right') {?>
+	<?php if (!@$options['nowrapper']) {?>
 	<div id="div_<?php echo get_class($element)?>_<?php echo $field?>" class="eventDetail"<?php if (@$htmlOptions['hide']) {?> style="display: none;"<?php }?>>
 		<?php if (!@$options['no-label']) {?>
 			<div class="label"></div>
 		<?php }?>
 		<div class="data">
+			<?php }?>
 			<?php echo CHtml::hiddenField(get_class($element)."[$field]",'0')?>
 			<?php echo CHtml::checkBox(get_class($element)."[$field]",$checked[$field],$htmlOptions)?>
 			<?php echo $element->getAttributeLabel($field)?>
