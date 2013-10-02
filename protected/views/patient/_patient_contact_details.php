158c8f63b6721473194dd2d20fd2d3a22ed35205
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-10-02 14:53:53 +0100

    [OE-3623] bugfix

--- protected/views/patient/_patient_contact_details.php
+++ protected/views/patient/_patient_contact_details.php
@@ -28,7 +28,7 @@
 	</div>
 	<div class="data_row">
 		<div class="data_label">Secondary telephone:</div>
-		<div class="data_value"><?php echo !empty($this->patient->secondary_phone) ? $this->patient->secondary_phone : 'None'?></div>
+		<div class="data_value"><?php echo !empty($this->patient->contact->secondary_phone) ? $this->patient->contact->secondary_phone : 'None'?></div>
 	</div>
 	<div class="data_row">
 		<div class="data_label">Email:</div>
