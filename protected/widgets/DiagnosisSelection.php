324ade663c00d9ac1763db84d00b2bf256724687
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-11-11 15:37:58 +0000

    fixed diagnosis selection when using a non-element model object.

--- protected/widgets/DiagnosisSelection.php
+++ protected/widgets/DiagnosisSelection.php
@@ -41,7 +41,7 @@ class DiagnosisSelection extends BaseFieldWidget
 	{
 		$this->class = get_class($this->element);
 		if (empty($_POST) || !array_key_exists($this->class, $_POST)) {
-			if (empty($this->element->event_id)) {
+			if (empty($this->element->id)) {
 				if ($this->default) {
 					// It's a new event so fetch the most recent element_diagnosis
 					$firmId = Yii::app()->session['selected_firm_id'];
