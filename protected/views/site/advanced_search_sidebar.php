2d2a95610de13911c05fef2188ecaf7775743cdb
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-11-27 09:21:46 +0000

    [OE-3760] adjusted advanced search sidebar

--- protected/views/site/advanced_search_sidebar.php
+++ protected/views/site/advanced_search_sidebar.php
@@ -1,19 +1,26 @@
 <div class="box admin">
-	<h2>Advanced search</h2>
+	<h2>Core</h2>
 	<ul class="navigation admin">
-		<?php
-		$uris = array('Core' => Yii::app()->createUrl('/site/advancedSearch'));
-		foreach (Yii::app()->params['advanced_search'] as $module) {
-			$uris[$module] = Yii::app()->createUrl("/$module/search/index");
-		}?>
-		<?php foreach ($uris as $title => $uri) {?>
-			<li<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?> class="selected"<?php }?>>
-				<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?>
-					<?php echo CHtml::link($title,array($uri),array('class' => 'selected'))?>
-				<?php } else {?>
-					<?php echo CHtml::link($title,array($uri))?>
-				<?php }?>
-			</li>
-		<?php }?>
 	</ul>
 </div>
+<?php foreach (Yii::app()->params['advanced_search'] as $module => $pages) {
+	if ($et = EventType::model()->find('class_name=?',array($module))) {
+		$name = $et->name;
+	} else {
+		$name = $module;
+	}?>
+	<div class="box admin">
+		<h2><?php echo $name?></h2>
+		<ul class="navigation admin">
+			<?php foreach ($pages as $title => $uri) {?>
+				<li<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?> class="selected"<?php }?>>
+					<?php if (Yii::app()->getController()->action->id == preg_replace('/^\/admin\//','',$uri)) {?>
+						<?php echo CHtml::link($title,Yii::app()->createUrl('/'.$module.'/search/'.$uri),array('class' => 'selected'))?>
+					<?php } else {?>
+						<?php echo CHtml::link($title,Yii::app()->createUrl('/'.$module.'/search/'.$uri))?>
+					<?php }?>
+				</li>
+			<?php }?>
+		</ul>
+	</div>
+<?php }?>
