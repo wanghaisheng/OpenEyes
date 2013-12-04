c8696bcfae83ab65905e5cf534b02720ec6dff59
Richard Willis <willis.rh@gmail.com>
2013-12-04 11:54:09 +0000

    Fixed dodgy whitespace introduced by rebase patch

--- docs/src/static-templates/components/admin/sidebar.php
+++ docs/src/static-templates/components/admin/sidebar.php
@@ -1,31 +1,31 @@
 <aside class="large-3 column sidebar admin">
 	<div class="box admin">
-                <h2>Section 1</h2>
-                <ul class="navigation admin">
-                        <li>
-                                <a href="#">A link</a>
-                        </li>
-                        <li>
-                                <a href="#" class="selected">Selected link</a>
-                        </li>
-                        <li>
-                                <a href="#">Another link</a>
-                        </li>
-                </ul>
-        </div>
+		<h2>Section 1</h2>
+		<ul class="navigation admin">
+			<li>
+				<a href="#">A link</a>
+			</li>
+			<li>
+				<a href="#" class="selected">Selected link</a>
+			</li>
+			<li>
+				<a href="#">Another link</a>
+			</li>
+		</ul>
+	</div>
 
-        <div class="box admin">
-                <h2>Section 2</h2>
-                <ul class="navigation admin">
-                        <li>
-                                <a href="#">A link</a>
-                        </li>
-                        <li>
-                                <a href="#">Selected link</a>
-                        </li>
-                        <li>
-                                <a href="#">Another link</a>
-                        </li>
-                </ul>
+	<div class="box admin">
+		<h2>Section 2</h2>
+		<ul class="navigation admin">
+			<li>
+				<a href="#">A link</a>
+			</li>
+			<li>
+				<a href="#">Selected link</a>
+			</li>
+			<li>
+				<a href="#">Another link</a>
+			</li>
+		</ul>
 	</div>
 </aside>
